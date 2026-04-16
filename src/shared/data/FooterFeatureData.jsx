510d25814008f7c7e8b177d84ce1608cb953b954
Duisenali06 <duisenali06@users.noreply.github.com>
2026-04-16 11:31:10 +0000

    Clear all files

--- src/shared/data/FooterFeatureData.jsx
+++
@@ -1,8 +0,0 @@
-import feature1 from '../../assets/icons/feature1.svg'
-import feature2 from '../../assets/icons/feature2.svg'
-import feature3 from '../../assets/icons/feature3.svg'
-import feature4 from '../../assets/icons/feature4.svg'
-import feature5 from '../../assets/icons/feature5.svg'
-const footerFeatureData = [feature1, feature2, feature3, feature4, feature5]
-
-export default footerFeatureData
